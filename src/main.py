0242b3c89a6645dff99886cbb9fd90cda98cf842
JakubLadkowski <115081571+JakubLadkowski@users.noreply.github.com>
2022-10-07 21:07:10 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,20 @@
+print("...:::Prosty Kalkulator:::...")
 
+x = int(input("Pierwsza cyfra:"))
+y = int(input("Druga cyfra:"))
+z = input("Co chcesz zrobic?(+,-,*,/,**)")
+
+operators = {"+": x+y, "-": x-y, "*": x*y, "/": x/y, "**": x**y}
+
+if z == "+":
+    print(operators["+"])
+elif z == "-":
+    print(operators["-"])
+elif z == "*":
+    print(operators["*"])
+elif z == "/":
+    print(operators["/"])
+elif z == "**":
+    print(operators["**"])
+else:
+    print("Error: )")
